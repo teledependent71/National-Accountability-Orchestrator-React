dd400fa51c7cb0c98178e084a35debc0934e411a
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2022-01-20 12:09:21 +0200

    Commit from TeeportHQ

---
+++ src/views/home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import styles from './home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles['container']}>
+      <Helmet>
+        <title>National Accountability Orchestrator</title>
+        <meta
+          property="og:title"
+          content="National Accountability Orchestrator"
+        />
+      </Helmet>
+    </div>
+  )
+}
+
+export default Home
